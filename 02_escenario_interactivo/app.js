3aba8e0bf6438089aac2e33ea2b72638cf871676
Caleboolaya17 <fernandoolaya086@gmail.com>
2025-10-27 17:18:41 -0500

    Add files via upload

--- 02_escenario_interactivo/app.js
+++ 02_escenario_interactivo/app.js
@@ -1,24 +1,4 @@
-const finit = document.querySelectorAll('.soldado');
-const enemigo = document.querySelectorAll('.enemigo');
-const salto = document.querySelectorAll('.salto');
-let counter = 0;
 
-console.log(finit, enemigo, salto);
-
-
-
-salto.forEach(item => {
-
-    item.addEventListener('click', () => {
-        item.classList.add('salto');   
-
-    });
-})
-
-const azul = document.querySelectorAll('#azul');
-const morado = document.querySelectorAll('#morado');
-const rojo = document.querySelectorAll('#rojo');
-const verde = document.querySelectorAll('#verde');
 
 
 
