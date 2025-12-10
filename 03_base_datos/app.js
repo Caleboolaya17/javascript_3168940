6de31e53b0bfff1977c3a7da2252950035b20c1d
Caleboolaya17 <fernandoolaya086@gmail.com>
2025-12-10 16:44:09 -0500

    Delete 03_base_datos directory

--- 03_base_datos/app.js
+++
@@ -1,8 +0,0 @@
-const tituloComic = document.querySelector('.titulo-comic');
-
-console.log(tituloComic);
-
-tituloComic.innerHTML = `
-            <h1>${comic.nombreComic}</h1>
-            
-            `
